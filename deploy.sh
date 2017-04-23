55a797a1455bb142e808f666239c88f8b489c774
valrus <imccowan@gmail.com>
2017-04-23 13:56:47 -0700

    Add lines for using unminified saveSvgAsPng

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 
 cp -f src/static/js/elm.min.js gh-pages/src/static/js
 cp -f src/static/js/undertale.min.js gh-pages/src/static/js
+cp -f src/static/js/saveSvgAsPng.min.js gh-pages/src/static/js
+# cp -f src/static/js/saveSvgAsPng.js gh-pages/src/static/js
 cp -f src/static/css/*.min.css gh-pages/src/static/css
